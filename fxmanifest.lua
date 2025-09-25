0ff715f837b36f406909324e2472892033303f3a
MaDHouSe <madhouse1979@gmail.com>
2025-09-25 09:56:44 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 games {'gta5'}
 author 'MaDHouSe79'
 description 'MH Vehicle Crash'
-version '1.0'
+version '1.0.0'
 client_scripts {'config.lua', 'client/main.lua'}
 server_scripts {'server/update.lua'}
 lua54 'yes'
